f91a8b8053335d561d32341a746ae5ed554daf49
pyunix <136034468+pyunix@users.noreply.github.com>
2024-03-29 18:56:25 +1300

    Prepare for PyPi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='sp-util',
+    version='0.0.1',
+    description='Special Projects - Utility Programs',
+    long_description='Utilities to manipulate various formats of files, gather inventory and generally do common things',
+    url='https://www.example.com',
+    author='pyunix',
+    author_email='me@example.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
